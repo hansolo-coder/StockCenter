fdb29d479ddc32c8c0fcdecbd0653a070a07753b
hansolo <hansolo@bizoir.dk>
2021-04-21 20:01:15 +0200

    Now also shows purchases and sales in last year on the Tax report form

--- classes/widgets/listTax.class.php
+++ classes/widgets/listTax.class.php
@@ -48,7 +48,7 @@
 			$forYear = date('Y') - 1;
 			print "<h1>Tax report (" . $forYear . ")</h1>\n";
 			print "<pre>\n";
-			print "<h2>DIVIDENDS per account\n";
+			print "<h2>DIVIDENDs per account\n";
 			print "=====================</h2>\n";
 			$totalCostAllOAccount = 0;
 			$totalTaxAllOAccount = 0;
@@ -56,23 +56,16 @@
 			$totalTaxAllPAccount = 0;
 			foreach($accounts as $row) {
 			  // get dividends for this account
-			  $sql = "SELECT t.tDate, t.tDateIsApprox, t.symbol, t.cost, t.tax, coalesce(t.currency, a.accountCurrency) AS currency, t.exchangeRate, a.accountCurrency AS ccurrency  FROM transactions t LEFT OUTER JOIN accounts a ON t.accountId = a.accountId WHERE t.activity = 'DIVIDEND' AND t.accountId = :accountId AND t.tDate BETWEEN '" . $forYear . "-01-01' AND '" . $forYear . "-12-31' ORDER BY t.symbol, t.tDate";
+			  $sql = "SELECT t.tDate, t.tDateIsApprox, t.symbol, t.cost, CASE WHEN tax <> '' THEN tax ELSE 0 END AS tax, coalesce(t.currency, a.accountCurrency) AS currency, t.exchangeRate, a.accountCurrency AS ccurrency  FROM transactions t LEFT OUTER JOIN accounts a ON t.accountId = a.accountId WHERE t.activity = 'DIVIDEND' AND t.accountId = :accountId AND t.tDate BETWEEN '" . $forYear . "-01-01' AND '" . $forYear . "-12-31' ORDER BY t.symbol, t.tDate";
 			  $rs = $db->prepare($sql);
 			  $rs->bindValue(':accountId', $row['accountId']);
 			  $rs->execute();
 			  $dividends = $rs->fetchAll();
 			  if (count($dividends) > 0) {
-			    if ($row['isPension'] == 'Y')
-			      print "<b>Pension account";
-			    else
-			      print "<b>Ordinary account";
-			    if (strlen($row['accountType']) > 0)
-			      print " (" . $row['accountType'] . ")";
-			    print "</b>\n<b>" . $row['accountNumber'] . " (" . $row['accountName'] . ") - " . $row['financialInstitution'];
-			    print "</b>\n";
-			    $maxLengthSymbol = $this->getMaxLength($dividends, 'symbol');
-			    $maxLengthCost = $this->getMaxLengthMoney($dividends, 'cost', 'currency', 'exchangeRate', 'ccurrency');
-			    $maxLengthTax = $this->getMaxLengthMoney($dividends, 'tax', 'currency', 'exchangeRate', 'ccurrency');
+			    $this->displayAccountInfo($row);
+			    $maxLengthSymbol = $this->getMaxLength($dividends, 'symbol', 'Symbol');
+			    $maxLengthCost = $this->getMaxLengthMoney($dividends, 'cost', 'currency', 'exchangeRate', 'ccurrency', 'Gross');
+			    $maxLengthTax = $this->getMaxLengthMoney($dividends, 'tax', 'currency', 'exchangeRate', 'ccurrency', 'Tax');
 			    $totalCost = 0;
 			    $totalTax = 0;
 			    print "<u>" . str_pad("Date",10) . " " . str_pad('Symbol', $maxLengthSymbol) . " " . str_pad('Gross', $maxLengthCost, " ", STR_PAD_LEFT);
@@ -120,11 +113,127 @@
 			if ($_SESSION['showTransactionTax'] == 'YES')
 			  print "All Pension  Account tax total: " . $this->formatCashWCurrLocal($totalTaxAllPAccount, $trow['ccurrency']) . "\n";
 			print "</h3>\n";
-			print "<h2>PURCHASE in year\n";
+
+			print "<h2>PURCHASEs in year\n";
 			print "================</h2>\n";
+			$totalCostAllOAccount = 0;
+			$totalFeeAllOAccount = 0;
+			$totalCostAllPAccount = 0;
+			$totalFeeAllPAccount = 0;
+			foreach($accounts as $row) {
+			  // get purchased shares for this account
+			  $sql = "SELECT t1.tDate, t1.tDateIsApprox, t1.symbol, t1.shares, t1.shares * t1.cost AS cost, CASE WHEN t1.tax <> '' THEN t1.tax ELSE 0 END AS tax, COALESCE(t1.currency, a.accountCurrency) AS currency, t1.exchangeRate, a.accountCurrency AS ccurrency, t2.cost AS costFEE, t2.tax AS taxFEE, t2.currency AS currencyFEE, t2.exchangeRate AS exchangeRateFEE FROM transactions t1 LEFT OUTER JOIN accounts a ON t1.accountId = a.accountId LEFT OUTER JOIN transactions t2 ON t1.symbol = t2.symbol AND t1.tDate = t2.tDate AND t1.accountId = t2.accountId AND t2.activity = 'FEE' WHERE t1.activity in ('BUY') AND t1.accountId = :accountId AND t1.tDate BETWEEN '" . $forYear . "-01-01' AND '" . $forYear . "-12-31' ORDER BY t1.symbol, t1.tDate";
+			  $rs = $db->prepare($sql);
+			  $rs->bindValue(':accountId', $row['accountId']);
+			  $rs->execute();
+			  $purchases = $rs->fetchAll();
+			  if (count($purchases) > 0) {
+			    $this->displayAccountInfo($row);
+			    $maxLengthSymbol = $this->getMaxLength($purchases, 'symbol', 'Symbol');
+			    $maxLengthShares = $this->getMaxLength($purchases, 'shares', 'Shares');
+			    $maxLengthCost = $this->getMaxLengthMoney($purchases, 'cost', 'currency', 'exchangeRate', 'ccurrency', 'Cost');
+			    $maxLengthFee = $this->getMaxLengthMoney($purchases, 'costFEE', 'currencyFEE', 'exchangeRateFEE', 'ccurrency', 'Fee');
+			    $totalCost = 0;
+			    $totalFee = 0;
+
+			    print "<u>" . str_pad("Date",10) . " " . str_pad('Symbol', $maxLengthSymbol) . " " . str_pad('Shares', $maxLengthShares) . " " . str_pad('Cost', $maxLengthCost, " ", STR_PAD_LEFT) . " " . str_pad('Fee', $maxLengthFee, " ", STR_PAD_LEFT) . "</u>\n";
+			    foreach($purchases as $trow) {
+			      // TODO format tDate to region - probably requires storing dates as integers globally (which is probably a good idea anyway)
+			      if (isset($_SESSION['region']) and $_SESSION['region'] == 'US') {
+			      //  print date("m-d-Y", $trow['tDate']);
+			        print $trow['tDate'];
+			      } else {
+			      //  print date("Y-m-d", $trow['tDate']);
+			        print $trow['tDate'];
+			      }
+			      print " " . str_pad($trow['symbol'], $maxLengthSymbol, " ");
+			      print " " . str_pad($trow['shares'], $maxLengthShares, " ");
+			      print " " . str_pad($this->formatCashWRateLocal($trow['cost'], $trow['currency'], $trow['exchangeRate'], $trow['ccurrency']), $maxLengthCost, " ", STR_PAD_LEFT);
+			      print " " . str_pad($this->formatCashWRateLocal($trow['costFEE'], $trow['currencyFEE'], $trow['exchangeRateFEE'], $trow['ccurrency']), $maxLengthFee, " ", STR_PAD_LEFT);
+			      print "\n";
+			      $totalCost += $trow['cost'] * $trow['exchangeRate'];
+			      $totalFee += $trow['costFEE'] * $trow['exchangeRateFEE'];
+			    }
+			    $totalCost = toCash($totalCost);
+			    $totalFee = toCash($totalFee);
+			    if ($row['isPension'] == 'Y') {
+			      $totalCostAllPAccount += $totalCost;
+			      $totalFeeAllPAccount += $totalFee;
+			    } else {
+			      $totalCostAllOAccount += $totalCost;
+			      $totalFeeAllOAccount += $totalFee;
+			    }
+			    print "<u>Account total    : " . $this->formatCashWCurrLocal($totalCost, $trow['ccurrency']) . "</u>\n";
+			    print "<u>Account fee total: " . $this->formatCashWCurrLocal($totalFee, $trow['ccurrency']) . "</u>\n";
+			    print "\n";
+			  }
+			}
+			print "<h3>All Ordinary Account total    : " . $this->formatCashWCurrLocal($totalCostAllOAccount, $trow['ccurrency']) . "\n";
+			print "All Ordinary Account fee total: " . $this->formatCashWCurrLocal($totalFeeAllOAccount, $trow['ccurrency']) . "\n";
+			print "All Pension  Account total    : " . $this->formatCashWCurrLocal($totalCostAllPAccount, $trow['ccurrency']) . "\n";
+			print "All Pension  Account fee total: " . $this->formatCashWCurrLocal($totalFeeAllPAccount, $trow['ccurrency']) . "\n";
+			print "</h3>\n";
+
 			print "\n";
-			print "<h2>SALE in year\n";
+			print "<h2>SALEs in year\n";
 			print "=============</h2>\n";
+			$totalCostAllOAccount = 0;
+			$totalFeeAllOAccount = 0;
+			$totalCostAllPAccount = 0;
+			$totalFeeAllPAccount = 0;
+			foreach($accounts as $row) {
+			  // get sold shares for this account
+			  $sql = "SELECT t1.tDate, t1.tDateIsApprox, t1.symbol, t1.shares, t1.shares * t1.cost AS cost, CASE WHEN t1.tax <> '' THEN t1.tax ELSE 0 END AS tax, COALESCE(t1.currency, a.accountCurrency) AS currency, t1.exchangeRate, a.accountCurrency AS ccurrency, t2.cost AS costFEE, t2.tax AS taxFEE, t2.currency AS currencyFEE, t2.exchangeRate AS exchangeRateFEE FROM transactions t1 LEFT OUTER JOIN accounts a ON t1.accountId = a.accountId LEFT OUTER JOIN transactions t2 ON t1.symbol = t2.symbol AND t1.tDate = t2.tDate AND t1.accountId = t2.accountId AND t2.activity = 'FEE' WHERE t1.activity in ('SELL') AND t1.accountId = :accountId AND t1.tDate BETWEEN '" . $forYear . "-01-01' AND '" . $forYear . "-12-31' ORDER BY t1.symbol, t1.tDate";
+			  $rs = $db->prepare($sql);
+			  $rs->bindValue(':accountId', $row['accountId']);
+			  $rs->execute();
+			  $sells = $rs->fetchAll();
+			  if (count($sells) > 0) {
+			    $this->displayAccountInfo($row);
+			    $maxLengthSymbol = $this->getMaxLength($sells, 'symbol', 'Symbol');
+			    $maxLengthShares = $this->getMaxLength($sells, 'shares', 'Shares');
+			    $maxLengthCost = $this->getMaxLengthMoney($sells, 'cost', 'currency', 'exchangeRate', 'ccurrency', 'Amount');
+			    $maxLengthFee = $this->getMaxLengthMoney($sells, 'costFEE', 'currencyFEE', 'exchangeRateFEE', 'ccurrency', 'Fee');
+			    $totalCost = 0;
+			    $totalFee = 0;
+
+			    print "<u>" . str_pad("Date",10) . " " . str_pad('Symbol', $maxLengthSymbol) . " " . str_pad('Shares', $maxLengthShares) . " " . str_pad('Amount', $maxLengthCost, " ", STR_PAD_LEFT) . " " . str_pad('Fee', $maxLengthFee, " ", STR_PAD_LEFT) . "</u>\n";
+			    foreach($sells as $trow) {
+			      // TODO format tDate to region - probably requires storing dates as integers globally (which is probably a good idea anyway)
+			      if (isset($_SESSION['region']) and $_SESSION['region'] == 'US') {
+			      //  print date("m-d-Y", $trow['tDate']);
+			        print $trow['tDate'];
+			      } else {
+			      //  print date("Y-m-d", $trow['tDate']);
+			        print $trow['tDate'];
+			      }
+			      print " " . str_pad($trow['symbol'], $maxLengthSymbol, " ");
+			      print " " . str_pad($trow['shares'], $maxLengthShares, " ");
+			      print " " . str_pad($this->formatCashWRateLocal($trow['cost'], $trow['currency'], $trow['exchangeRate'], $trow['ccurrency']), $maxLengthCost, " ", STR_PAD_LEFT);
+			      print " " . str_pad($this->formatCashWRateLocal($trow['costFEE'], $trow['currencyFEE'], $trow['exchangeRateFEE'], $trow['ccurrency']), $maxLengthFee, " ", STR_PAD_LEFT);
+			      print "\n";
+			      $totalCost += $trow['cost'] * $trow['exchangeRate'];
+			      $totalFee += $trow['costFEE'] * $trow['exchangeRateFEE'];
+			    }
+			    $totalCost = toCash($totalCost);
+			    $totalFee = toCash($totalFee);
+			    if ($row['isPension'] == 'Y') {
+			      $totalCostAllPAccount += $totalCost;
+			      $totalFeeAllPAccount += $totalFee;
+			    } else {
+			      $totalCostAllOAccount += $totalCost;
+			      $totalFeeAllOAccount += $totalFee;
+			    }
+			    print "<u>Account total    : " . $this->formatCashWCurrLocal($totalCost, $trow['ccurrency']) . "</u>\n";
+			    print "<u>Account fee total: " . $this->formatCashWCurrLocal($totalFee, $trow['ccurrency']) . "</u>\n";
+			    print "\n";
+			  }
+			}
+			print "<h3>All Ordinary Account total    : " . $this->formatCashWCurrLocal($totalCostAllOAccount, $trow['ccurrency']) . "\n";
+			print "All Ordinary Account fee total: " . $this->formatCashWCurrLocal($totalFeeAllOAccount, $trow['ccurrency']) . "\n";
+			print "All Pension  Account total    : " . $this->formatCashWCurrLocal($totalCostAllPAccount, $trow['ccurrency']) . "\n";
+			print "All Pension  Account fee total: " . $this->formatCashWCurrLocal($totalFeeAllPAccount, $trow['ccurrency']) . "\n";
+			print "</h3>\n";
 /*
 select * from transactions where tDate between '2020-01-01' and '2021-12-31' and activity in ('SELL', 'FEE');
 
@@ -146,98 +255,28 @@ where t1.tDate between '2020-01-01' and '2021-12-31' and t1.activity in ('SELL',
 			print "</pre>\n";
 		}
 
-		
-		/**
-		 * validates the form
-		 */
-		function check($symbol)
-		{
-			# symbol
-			if(isset($symbol))
-			{
-				// TODO Perhaps makes this optional by choosing between US and Europe setup
-				# if symbol is not alpha/numeric only...
-				#if(!ctype_alnum(trim($symbol)))
-				#{
-				#	# invalid characters error
-				#	$this->errors .= "Invalid characters in stock symbol<br>";
-				#}
-			}
-			else 
-			{
-				# missing symbol error
-				$this->errors .= "Stock symbol required<br>";
-			}
-		}
-
-		
-		/**
-		 * processes the form
-		 */
-		function process()
-		{
-			$symbol = NULL;
-			$ISIN = NULL;
-			$name = NULL;
-
-			$arr = explode("/", $_REQUEST['symbol']);
-			$arr2 = explode("\\", $_REQUEST['symbol']);
-			if (count($arr) == 3) {
-				$symbol = trim($arr[0]);
-				$ISIN = trim($arr[1]);
-				$name = trim($arr[2]);
-			} elseif (count($arr2) == 3) {
-				$symbol = trim($arr2[0]);
-				$ISIN = trim($arr2[1]);
-				$name = trim($arr2[2]);
-                        } else {
-				$symbol = $_REQUEST['symbol'];
-			}
-			$this->check($symbol);
-			
-			# if there are no errors...
-			if($this->errors == "")
-			{
-				include_once './classes/tc/stocks.class.php';
-				
-				# add the stock symbol
-				$stock = new stocks();
-				$stock->symbol = $symbol;
-				$stock->ISIN = $ISIN;
-				$stock->name = $name;
-				$stock->insert();
-				
-				# get data for symbol from yahoo
-				getData($symbol);
-				
-				# display a success message and the home page
-				message("success", "Stock symbol (" . trim(strtoupper($symbol)) . ") added");
-				homePage();
-			}
-			else
-			{
-				# display errors and the home page
-				message("error", $this->errors);
-			}
-		}
-
-		function getMaxLength($dividends, $column) {
+		function getMaxLength($rows, $column, $heading) {
 			$maxLength = 0;
-			foreach($dividends as $trow) {
+			foreach($rows as $trow) {
 				$thisLength = strlen($trow[$column]);
 				if ($thisLength > $maxLength)
 					$maxLength = $thisLength;
 			}
+			if (strlen($heading) > $maxLength)
+			  $maxLength = strlen($heading);
 			return $maxLength;
 		}
-		function getMaxLengthMoney($dividends, $columnMoney, $currency, $rateColumn, $rateCurrency) {
+
+		function getMaxLengthMoney($rows, $columnMoney, $currency, $rateColumn, $rateCurrency, $heading) {
 			$maxLength = 0;
-			foreach($dividends as $trow) {
+			foreach($rows as $trow) {
 				$line = $this->formatCashWRateLocal($trow[$columnMoney], $trow[$currency], $trow[$rateColumn], $trow[$rateCurrency]);
 				$thisLength = mb_strlen($line);
 				if ($thisLength > $maxLength)
 					$maxLength = $thisLength;
 			}
+			if (strlen($heading) > $maxLength)
+			  $maxLength = strlen($heading);
 			return $maxLength;
 		}
 
@@ -258,6 +297,17 @@ where t1.tDate between '2020-01-01' and '2021-12-31' and t1.activity in ('SELL',
 			  return number_format((float)$value, 2, $_SESSION['DecimalPoint'], $_SESSION['ThousandSep']) . " " . $currency;
 			else
 			  return $currency . " " . number_format((float)$value, 2, $_SESSION['DecimalPoint'], $_SESSION['ThousandSep']);
+		}
+
+		function displayAccountInfo($accountRow) {
+			if ($accountRow['isPension'] == 'Y')
+			  print "<b>Pension account";
+			else
+			  print "<b>Ordinary account";
+			if (strlen($accountRow['accountType']) > 0)
+			  print " (" . $accountRow['accountType'] . ")";
+			print "</b>\n<b>" . $accountRow['accountNumber'] . " (" . $accountRow['accountName'] . ") - " . $accountRow['financialInstitution'];
+			print "</b>\n";
 		}
 
 	}
